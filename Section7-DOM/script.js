f8c3b55d5886009104dde7ad8cae6e547ece5d68
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-13 17:18:10 +0300

    Logic about guessing the number was rewritten.

--- Section7-DOM/script.js
+++ Section7-DOM/script.js
@@ -18,17 +18,28 @@ document.querySelector('.check').addEventListener('click', function () {
         if (score > highscore) {
             highscore = score;3
             document.querySelector('.highscore').textContent = highscore;
+        } 
+    } 
+    else if(guess !== number) {
+        if(score > 1){
+            document.querySelector('.message').textContent = guess > number ? 'Too high' : 'Too low';
+            score--;
+            document.querySelector('.score').textContent = score;
         }
-
-    } else if (guess > number) {
-        document.querySelector('.message').textContent = 'Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    } else if (guess < number) {
-        document.querySelector('.message').textContent = 'Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
+        else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
     }
+    // else if (guess > number) {
+    //     document.querySelector('.message').textContent = 'Too high!';
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    // } else if (guess < number) {
+    //     document.querySelector('.message').textContent = 'Too low!';
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
