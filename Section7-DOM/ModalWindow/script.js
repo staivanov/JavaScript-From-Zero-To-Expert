a0e15db7bb87ab28ecaecaa379c48687947a578f
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-13 18:25:39 +0300

    Open and close windows was implemented.

--- Section7-DOM/ModalWindow/script.js
+++ Section7-DOM/ModalWindow/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnOpenModal);
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let index = 0; index < btnOpenModal.length; index++) {
+    btnOpenModal[index].addEventListener('click', openModal);
+}
+
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
