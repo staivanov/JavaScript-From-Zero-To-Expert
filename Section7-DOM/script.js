62d1a6c99a8682f7df8b9ec94167d1ad14ba773f
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-13 16:41:34 +0300

    Guess the nomer logic was implemented.

--- Section7-DOM/script.js
+++ Section7-DOM/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === number) {
+
+        document.querySelector('body').style.backgroundColor = '#40b365';
+        document.querySelector('.message').textContent = 'Correct number.';
+        document.querySelector('.number').textContent = number;
+
+
+    } else if (guess > number) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < number) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+
+
+});
+
