b4df05ea21b3fa05e75c163b510356761c7c7cba
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-14 10:58:12 +0300

    Demo for every of the three scopes.

--- Section8-HowJSwork/scopes.js
+++ Section8-HowJSwork/scopes.js
@@ -1,4 +1,32 @@
 //Scope is the place where variables are declared.
 //Scopes in JS are: global scope, function scope, block scope
 
+//Global scope. Variables in global scope are accessible everywhere
+const me = 'Marcus';
+const job = 'teacher';
+const born = 1988;
 
+//Function scope. Variables declared in function scope are accessible only there.
+function calculateAge(yearsOfBirth) {
+    let currentYear = 2024;
+    let age = currentYear - yearsOfBirth;
+
+    return age;
+}
+
+//Block scope. Only 'let' and 'const' are restricted to block scope.
+
+const currentAge = calculateAge(born);
+
+if (currentAge > 18) {
+    console.log('You can enter in every club.');
+
+    let drinks = ['Vodka', 'Rakia', 'Beer', 'Whiskey'];
+
+    drinks.forEach(aperitif => {
+        console.log(aperitif);
+    });
+}
+else {
+    console.log('You can\'t enter in any club.');
+}
