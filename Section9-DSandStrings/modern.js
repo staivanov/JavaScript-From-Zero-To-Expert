14855f4cc279ae311137402a96e4fad9b56b6ec4
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-17 11:07:05 +0300

    Rest pattern and function was implemented.

--- Section9-DSandStrings/modern.js
+++ Section9-DSandStrings/modern.js
@@ -24,8 +24,32 @@ const books =
     highlighted: true
 };
 
+const [first, second] = books.keywords;
+console.log(first, second);
 
-const  arr = [2, 3, 4];
-const [x, y,z] = arr;
+//Spread operator
+let newKeywords = [...books.keywords, 'ORM'];
+newKeywords[newKeywords.length - 1] = 'JavaScript';
+console.log(newKeywords);
+console.log(books.keywords);
 
-console.log(x, y, z)
+//Rest pattern
+const [a, b, ...other] = [1, 2, 3, 4, 5,6];
+console.log(a, b, other);
+
+
+//Functions
+const addNumbers = function(...numbers) {
+
+    let sum = 0;
+
+    numbers.forEach(number => {
+        sum += number;
+    });
+
+        return sum;
+};
+
+addNumbers(1);
+addNumbers(2, 3);
+addNumbers(99, 14, 83);
