29ff041a5b3bfa2a0b887fdf7dcc273c12ef3996
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-17 11:01:24 +0300

    Rest pattern was added.

---
+++ Section9-DSandStrings/modern.js
@@ -0,0 +1,31 @@
+const books =
+{
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: ['computer science', 'programming', 'algorithms', 'data structures', 'java', 'math', 'software', 'engineering'],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+        goodreads: {
+            rating: 4.41,
+            ratingsCount: 1733,
+            reviewsCount: 63,
+            fiveStarRatingCount: 976,
+            oneStarRatingCount: 13
+        }
+    },
+    highlighted: true
+};
+
+
+const  arr = [2, 3, 4];
+const [x, y,z] = arr;
+
+console.log(x, y, z)
