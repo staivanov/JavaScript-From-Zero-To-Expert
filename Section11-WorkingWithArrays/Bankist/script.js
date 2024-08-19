082fa3a0b8fd34d7a3c56b700a93bdb2e0c6ac43
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-19 17:00:20 +0300

    Demo for filter method.

--- Section11-WorkingWithArrays/Bankist/script.js
+++ Section11-WorkingWithArrays/Bankist/script.js
@@ -61,6 +61,29 @@ const checkDogs = function (dogsJulia, dogsKate) {
 };
 
 displayMovements(account1.movements);
-console.log(containerMovements.innerHtml = ' ');
 
-//Data transofrmations: map, filter, reduce
+
+const createUsername = function (accs) {
+
+  accs.forEach(function (acc) {
+    acc.username = user.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join(' ');
+  });
+
+};
+
+const user = 'Tommy Lee';
+console.log(createUsername(user));
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(deposits);
+console.log(movements);
+
+const withdrawal = movements.filter(mov => mov < 0);
+console.log(withdrawal);
