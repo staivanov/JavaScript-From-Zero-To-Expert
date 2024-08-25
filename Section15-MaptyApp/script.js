ee00f0c8e2426e65a12bad04cd6e60a6155449af
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2024-08-25 11:57:18 +0300

    Displaying a map maker.

--- Section15-MaptyApp/script.js
+++ Section15-MaptyApp/script.js
@@ -27,13 +27,28 @@ if (navigator.geolocation) {
         const coords = [latitude, longitude];
         const map = L.map('map').setView(coords, 13);
 
+
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-            .openPopup();
+        map.on('click', function (mapEvent) {
+            console.log(mapEvent);
+            const { lat, lng } = mapEvent.latlng;
+
+            L.marker([lat, lng])
+                .addTo(map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 200,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-pop',
+                    }))
+                .setPopupContent('Workout')
+                .openPopup();
+        });
     });
 }
 
